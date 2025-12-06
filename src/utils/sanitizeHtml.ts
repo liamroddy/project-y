025f1a6b54ae11d308138a0dd90a38ca22109d4b
Liam Roddy <38569656+liamroddy@users.noreply.github.com>
2025-12-06 22:27:27 +0000

    prettier change array

--- src/utils/sanitizeHtml.ts
+++ src/utils/sanitizeHtml.ts
@@ -1,7 +1,20 @@
 import DOMPurify from 'dompurify';
 
 const sanitizerConfig = {
-  ALLOWED_TAGS: ['a', 'blockquote', 'br', 'code', 'em', 'i', 'li', 'ol', 'p', 'pre', 'strong', 'ul'],
+  ALLOWED_TAGS: [
+    'a',
+    'blockquote',
+    'br',
+    'code',
+    'em',
+    'i',
+    'li',
+    'ol',
+    'p',
+    'pre',
+    'strong',
+    'ul',
+  ],
   ALLOWED_ATTR: ['href', 'rel', 'target', 'title'],
   USE_PROFILES: { html: true },
 };
